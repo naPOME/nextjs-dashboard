5f8dace1c97e986024b3d49398e3c039d579d6d7
naPOME <natipomebiz1234@gmail.com>
2024-08-15 19:12:08 +0300

    invoice final

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,7 +26,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  deleteInvoiceWithId = deleteInvoice.bind(null,id);
+  const deleteInvoiceWithId = deleteInvoice.bind(null,id);
   return (
     <>
     <form action={deleteInvoiceWithId}>
